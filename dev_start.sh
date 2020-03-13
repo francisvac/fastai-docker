71631d5bbfbd23205f4e1d83f8c02bf77e264146
Francis Joseph <francisvac@gmail.com>
2020-03-12 21:05:51 -0700

    volume mapping

--- dev_start.sh
+++ dev_start.sh
@@ -1,5 +1,5 @@
 docker run -it -d \
-     -v $HOME/fastai:/data/fastai \
+     -v $HOME/fastai-docker:/data/fastai-docker \
      --name docker-fastai  \
      -e NVIDIA_VISIBLE_DEVICES=all \
      -p 8888:8888 \
