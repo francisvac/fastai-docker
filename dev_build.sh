f080633e2a2020493d5529e924b2de30691d7c33
Francis Joseph <francisvac@gmail.com>
2020-03-12 21:04:44 -0700

    clone repo

--- dev_build.sh
+++ dev_build.sh
@@ -1,3 +1,4 @@
-docker build -t fastai /home/francis/fastai/docker/fastai-docker/fastai-v3
-
-
+cd ~
+git clone https://github.com/Paperspace/fastai-docker.git
+cd fastai-docker/fastai-v3
+docker build -t fastai .
